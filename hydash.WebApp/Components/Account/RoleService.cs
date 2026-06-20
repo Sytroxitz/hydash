7ea880e38d7a80e3f39ad184a2b40a64dff20c43
$lxy Txz <68563594+Sytroxitz@users.noreply.github.com>
2026-06-20 03:55:14 +0200

    add role permission methods to RoleService

--- hydash.WebApp/Components/Account/RoleService.cs
+++ hydash.WebApp/Components/Account/RoleService.cs
@@ -1,7 +1,10 @@
 ﻿using hydash.WebApp.Data;
+using hydash.WebApp.Data.EF;
 using hydash.WebApp.Data.Enums;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace hydash.WebApp.Components.Account
@@ -10,11 +13,13 @@ namespace hydash.WebApp.Components.Account
     {
         private readonly RoleManager<ApplicationRole> _roleManager;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IDbContextFactory<ApplicationDbContext> _contextFactory;
 
-        public RoleService(RoleManager<ApplicationRole> roleManager, UserManager<ApplicationUser> userManager)
+        public RoleService(RoleManager<ApplicationRole> roleManager, UserManager<ApplicationUser> userManager, IDbContextFactory<ApplicationDbContext> contextFactory)
         {
             _roleManager = roleManager;
             _userManager = userManager;
+            _contextFactory = contextFactory;
         }
 
         // Create a role if it doesn't exist
@@ -78,6 +83,44 @@ namespace hydash.WebApp.Components.Account
             }
 
             return highestPriorityRole;
+        }
+
+        // Update an existing role
+        public async Task UpdateRoleAsync(ApplicationRole role)
+        {
+            await _roleManager.UpdateAsync(role);
+        }
+
+        // Get the permission ids currently assigned to a role
+        public async Task<List<int>> GetRolePermissionIdsAsync(string roleId)
+        {
+            await using var context = await _contextFactory.CreateDbContextAsync();
+            return await context.RolePermissions
+                .Where(rp => rp.RoleId == roleId)
+                .Select(rp => rp.PermissionId)
+                .ToListAsync();
+        }
+
+        // Replace the set of permissions assigned to a role
+        public async Task SetRolePermissionsAsync(string roleId, IEnumerable<int> permissionIds)
+        {
+            await using var context = await _contextFactory.CreateDbContextAsync();
+
+            var existing = await context.RolePermissions
+                .Where(rp => rp.RoleId == roleId)
+                .ToListAsync();
+            context.RolePermissions.RemoveRange(existing);
+
+            foreach (var permissionId in permissionIds.Distinct())
+            {
+                context.RolePermissions.Add(new RolePermission
+                {
+                    RoleId = roleId,
+                    PermissionId = permissionId
+                });
+            }
+
+            await context.SaveChangesAsync();
         }
 
         // Add other role-related methods as needed
